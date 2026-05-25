d6bcc0c63cfd1e6e5222e9a3d8362e6297b31a6c
Nhlanhla Malaza <njmcloud@gmail.com>
2026-05-25 21:37:44 +0200

    action update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Some environments have issues with the default minifier worker
+    if (config.optimization && config.optimization.minimizer) {
+      config.optimization.minimizer.forEach((minimizer: any) => {
+        if (minimizer.options && minimizer.options.worker !== undefined) {
+          minimizer.options.worker = false;
+        }
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
